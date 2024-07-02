474bf5e37a227266dd08ad6e48b2ab6003ac0cc5
vrajSpatel <VSPATEL012004@GMAIL.COM>
2024-07-02 15:26:31 +0530

    pull main

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ChooseGoals from "./Pages/ChooseGoals";
 import SignIn from "./Pages/SignIn.jsx";
 import SigninwithPhone from "./Pages/SigninwithPhone.jsx";
 import Forgotpass from "./Component/Forgotpass.jsx";
+import Mentor from "./Pages/Mentor.jsx";
+import DownloadApp from "./Pages/DownloadApp.jsx";
 import Jobs from "./Pages/Jobs.js";
 import RecommendedJobs from "./Pages/RecommendedJobs.js";
 import Messanger from "./Context/Messanger.js";
@@ -15,23 +17,28 @@ import Account from "./Pages/Account.js";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" Component={Home} />
-          <Route path="/choose-goals" element={<ChooseGoals page={1} />} />
-          <Route
-            path="/choose-industries"
-            element={<ChooseGoals page={2} />}
-          />
-          <Route
-            path="/choose-roles"
-            element={<ChooseGoals page={3} />}
-          />
-          <Route path="/signin" Component={SignIn} />
-          <Route path="/signin_phone" Component={SigninwithPhone} />
-          <Route path="/forgotpassword" Component={Forgotpass} />
-        </Routes>
-      </BrowserRouter>
+      <Messanger>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" Component={Home} />
+            <Route path="/choose-goals" element={<ChooseGoals page={1} />} />
+            <Route path="/choose-industries" element={<ChooseGoals page={2} />}/>
+            <Route path="/choose-roles" element={<ChooseGoals page={3} />} />
+            <Route path="/process-profile" element={<ChooseGoals page={4} />} />
+            <Route path="/jobs" element={<Jobs />} />
+            <Route path="/post-a-job" element={<PostaJob />} />
+            <Route path="/recommended-jobs/*" element={<RecommendedJobs />} />
+            <Route path="/signin" Component={SignIn} />
+            <Route path="/account" Component={Account} />
+            <Route path="/signin_phone" Component={SigninwithPhone} />
+            <Route path="/forgotpassword" Component={Forgotpass} />
+            <Route path="/downloadapp" Component={DownloadApp} />
+            <Route path="/mentors" Component={Mentor}/>
+
+          </Routes>
+        </BrowserRouter>
+      </Messanger>
+      
     </>
   );
 }
